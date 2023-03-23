c6b4174827355d4ee8d152c0ae348138af974a66
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-23 19:03:07 +0100

    completed exercise

---
+++ OppgaveKapittelTreSolvingProblemsWithArrays/Cipher.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace OppgaveKapittelTreSolvingProblemsWithArrays
+{
+    internal class Cipher
+    {
+        string Alphabet;
+        string CurrentCipher;
+        // Constructor if no cipher is passed in - generates random cipher
+        public Cipher()
+        {
+            Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            CurrentCipher = GetRandomizedCipher();
+        }
+        // Constructor if user wants to input cipher
+        public Cipher(string cipher)
+        {
+            Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            CurrentCipher = cipher;
+        }
+        private static string GetRandomizedCipher()
+        {
+            var random = new Random();
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var cipher = "";
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                while (true)
+                {
+                    var randomCharacter = alphabet[random.Next(0, alphabet.Length)];
+
+                    if (!cipher.Contains(randomCharacter))
+                    {
+                        cipher += randomCharacter;
+                        break;
+                    }
+                }
+            }
+            return cipher;
+        }
+        public string Encrypt(string plainText)
+        {
+            string ciphertext = ConvertText(plainText, Alphabet, CurrentCipher);
+
+            return ciphertext;
+        }
+        public string Decrypt(string ciphertext)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string cipher = "IJVWXKLMNOPABCDYZEFGHQRSTU";
+
+            string plainText = ConvertText(ciphertext, CurrentCipher, Alphabet);
+
+            return plainText;
+        }
+        private string ConvertText(string inputText, string inputCharacterMap, string outputCharacterMap)
+        {
+            string outputText = "";
+
+            foreach (var character in inputText.ToUpperInvariant())
+            {
+                for (int i = 0; i < inputCharacterMap.Length; i++)
+                {
+                    if (character == inputCharacterMap[i]) outputText += outputCharacterMap[i];
+                }
+            }
+            return outputText;
+        }
+    }
+}
+
--- OppgaveKapittelTreSolvingProblemsWithArrays/Program.cs
+++ OppgaveKapittelTreSolvingProblemsWithArrays/Program.cs
@@ -1,13 +1,37 @@
-﻿namespace OppgaveKapittelTreSolvingProblemsWithArrays;
+﻿using System;
+
+namespace OppgaveKapittelTreSolvingProblemsWithArrays;
 class Program
 {
     static void Main(string[] args)
     {
+        /*
+         * Oppgave 3-3
+         */
         var sortedArray = new int[] { 1, 2, 3 };
         var unSortedArray = new int[] { 16, 222, 3, 44 };
         var arrayIsSorted = OppgaveTre(unSortedArray, 3);
+
+        Console.WriteLine("Sorting exercise:");
         Console.WriteLine($"int[] {{ 1, 2, 3 }} is a sorted array = {OppgaveTre(sortedArray,3)}");
         Console.WriteLine($"int[] {{ 16, 222, 3, 44 }} is a sorted array = {OppgaveTre(unSortedArray, 4)}");
+        Console.WriteLine();
+
+        /*
+         * Oppgave 3-4 & 3-5
+         */
+        var UserCipher = new Cipher("IJVWXKLMNOPABCDYZEFGHQRSTU");
+
+        Console.WriteLine("Cipher exercises:");
+        Console.WriteLine($"The encryption of GETACADEMY is: {UserCipher.Encrypt("GETACADEMY")}");
+        Console.WriteLine($"The decryption of LXGIVIWXBT is: {UserCipher.Decrypt("LXGIVIWXBT")}");
+        /*
+         * Oppgave 3-6
+         */
+        var RandomizedCipherOne = new Cipher();
+        var RandomizedCipherTwo = new Cipher();
+        Console.WriteLine($"First randomized cipher of GETACADEMY: {RandomizedCipherOne.Encrypt("GETACADEMY")}");
+        Console.WriteLine($"Second randomized cipher of GETACADEMY: {RandomizedCipherTwo.Encrypt("GETACADEMY")}");
     }
     
     static bool OppgaveTre(int[] array, int numberOfElements)
