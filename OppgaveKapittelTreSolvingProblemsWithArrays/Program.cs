49df5760da5a516b10d4ac9cecb75a3097be2e25
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-23 17:14:20 +0100

    oppgave 3-3

--- OppgaveKapittelTreSolvingProblemsWithArrays/Program.cs
+++ OppgaveKapittelTreSolvingProblemsWithArrays/Program.cs
@@ -3,7 +3,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var sortedArray = new int[] { 1, 2, 3 };
+        var unSortedArray = new int[] { 16, 222, 3, 44 };
+        var arrayIsSorted = OppgaveTre(unSortedArray, 3);
+        Console.WriteLine($"int[] {{ 1, 2, 3 }} is a sorted array = {OppgaveTre(sortedArray,3)}");
+        Console.WriteLine($"int[] {{ 16, 222, 3, 44 }} is a sorted array = {OppgaveTre(unSortedArray, 4)}");
+    }
+    
+    static bool OppgaveTre(int[] array, int numberOfElements)
+    {
+        /*
+         * 3-3 Write a bool function that is passed an array and the number of elements in
+         * that array and determines whether the data in the array is sorted. This should
+         * require only one pass!
+         */
+
+        int start = 0;
+        int end = numberOfElements - 1;
+        for (int i = start + 1; i <= end; i++)
+        {
+            for (int j = i; j > start && array[j - 1] > array[j]; j--)
+            {
+                int temp = array[j - 1];
+                array[j - 1] = array[j];
+                array[j] = temp;
+
+                return false;
+            }
+        }
+        return true;
     }
 }
-
